3bf3fa7b6d628bf3a0289cbcdac26b8e2512b711
ruidinispinheirodias <1rui@live.com.pt>
2025-05-14 15:43:19 +0100

    Add files via upload

---
+++ server.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const cors = require("cors");
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(cors());
+app.use(express.json());
+
+let dadosVitais = {};
+
+app.post("/vitais", (req, res) => {
+  dadosVitais = req.body;
+  console.log("Dados recebidos:", dadosVitais);
+  res.status(200).json({ mensagem: "Dados recebidos com sucesso" });
+});
+
+app.get("/vitais", (req, res) => {
+  res.json(dadosVitais);
+});
+
+app.listen(port, () => {
+  console.log(`Servidor a correr em http://localhost:${port}`);
+});
